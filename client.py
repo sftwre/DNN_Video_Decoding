e9328674054f33178cbace66f8893cd14345cacd
Isaac Buitrago <mightyking7@gmail.com>
2021-11-07 13:36:51 -0600

    loaded first clip from REDS dataset

--- client.py
+++ client.py
@@ -1,17 +1,26 @@
+import os
 import cv2
 import numpy as np
 import socket
 import struct
 from io import BytesIO
+from config import Config
 
-# Capture frame
-cap = cv2.VideoCapture(0)
+# modified code from https://github.com/rena2damas/remote-opencv-streaming-live-video
+
+config = Config()
+
+# clip to stream
+video_pth = os.path.join(config.REDS_PTH, '000')
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 8080))
 
-while cap.isOpened():
-    _, frame = cap.read()
+# load each frame in clip and send to server
+for frame_pth in os.listdir(video_pth):
+
+    frame = cv2.imread(frame_pth)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     memfile = BytesIO()
     np.save(memfile, frame)
@@ -23,5 +32,3 @@ while cap.isOpened():
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-cap.release()
