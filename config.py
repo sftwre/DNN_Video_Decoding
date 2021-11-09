e2fa718e3a2dd650bed0512aaec89e7406f4c5cf
Isaac Buitrago <mightyking7@gmail.com>
2021-11-08 19:15:52 -0600

    changed reds_pth to absolute path

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 class Config:
 
     def __init__(self):
-        self.REDS_PTH = './REDS_dataset/train_sharp_bicubic/X4'
+        self.REDS_PTH = '/Users/isaacbuitrago/PycharmProjects/remote-opencv-streaming-live-video/REDS_dataset/train_sharp_bicubic/X4'
