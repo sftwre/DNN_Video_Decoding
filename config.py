8233ae2b1269da8451d55c9ca3de11ee997c14ba
Isaac Buitrago <mightyking7@gmail.com>
2021-11-10 21:31:01 -0600

    added mp4 video

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 
 
 class Config:
+    """
+    Configuration file for project with paths to video assets and GCP compute IP addresses
+    """
 
     def __init__(self):
-        self.REDS_PTH = '/Users/isaacbuitrago/PycharmProjects/remote-opencv-streaming-live-video/REDS_dataset/train_sharp_bicubic/X4'
+        self.REDS_PTH = './REDS_dataset/train_sharp_bicubic/X4'
+        self.NYC_VIDEO_PTH = './video/NYCIntersection-20sec.mp4'
